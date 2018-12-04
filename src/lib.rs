66a41df5077daa9cb9d7aa34bab9b9e22effdd79
Bownairo <eerokelly@gmail.com>
2018-12-04 12:21:44 -0500

    Create new output structure, and mostly build out display

--- src/lib.rs
+++ src/lib.rs
@@ -26,6 +26,37 @@ impl Points {
     fn new() -> Self {
         Points { x: Vec::new(), y: Vec::new() }
     }
+
+    fn add(&mut self, x: i32, y: i32) {
+        self.x.push(x);
+        self.y.push(y);
+    }
+
+    fn len(&self) -> u32 {
+        self.x.len() as u32
+    }
+}
+
+struct Segs {
+    x: Vec<i32>,
+    y: Vec<i32>,
+}
+
+impl Segs {
+    fn new() -> Self {
+        Segs { x: Vec::new(), y: Vec::new() }
+    }
+
+    fn add(&mut self, x1: i32, y1: i32, x2: i32, y2: i32) {
+        self.x.push(x1);
+        self.x.push(x2);
+        self.y.push(y1);
+        self.y.push(y2);
+    }
+
+    fn len(&self) -> u32 {
+        (self.x.len() / 2) as u32
+    }
 }
 
 enum Status {
@@ -65,25 +96,37 @@ impl Input {
     }
 }
 
+struct Output {
+    points: Points,
+    segs: Segs,
+    rays: Segs,
+}
+
+impl Output {
+    fn new() -> Self {
+        Output { points: Points::new(), segs: Segs::new(), rays: Segs::new() }
+    }
+}
+
 #[wasm_bindgen]
 pub struct Core {
     config: Config,
     input: Input,
+    output: Output,
 }
 
 #[wasm_bindgen]
 impl Core {
     pub fn new() -> Self {
-        Core { config: Config::new(), input: Input::new() }
+        Core { config: Config::new(), input: Input::new(), output: Output::new() }
     }
 
     pub fn input_add_point(&mut self, x: i32, y: i32) {
-        self.input.points.x.push(x);
-        self.input.points.y.push(y);
+        self.input.points.add(x, y);
     }
 
     pub fn input_length(&self) -> u32 {
-        self.input.points.x.len() as u32
+        self.input.points.len()
     }
 
     pub fn input_points_x(&self) -> *const i32 {
@@ -92,6 +135,42 @@ impl Core {
 
     pub fn input_points_y(&self) -> *const i32 {
         self.input.points.y.as_ptr()
+    }
+
+    pub fn points_length(&self) -> u32 {
+        self.output.points.len()
+    }
+
+    pub fn points_x(&self) -> *const i32 {
+        self.output.points.x.as_ptr()
+    }
+
+    pub fn points_y(&self) -> *const i32 {
+        self.output.points.y.as_ptr()
+    }
+
+    pub fn segs_length(&self) -> u32 {
+        self.output.segs.len()
+    }
+
+    pub fn segs_x(&self) -> *const i32 {
+        self.output.segs.x.as_ptr()
+    }
+
+    pub fn segs_y(&self) -> *const i32 {
+        self.output.segs.y.as_ptr()
+    }
+
+    pub fn rays_length(&self) -> u32 {
+        self.output.rays.len()
+    }
+
+    pub fn rays_x(&self) -> *const i32 {
+        self.output.rays.x.as_ptr()
+    }
+
+    pub fn rays_y(&self) -> *const i32 {
+        self.output.rays.y.as_ptr()
     }
 
     pub fn halt(&self) -> *const u8 {
--- www/index.js
+++ www/index.js
@@ -14,6 +14,7 @@ const GREEN = "#00FF00";
 const BLUE = "#0000FF";
 
 const POINT_COLOR = RED;
+const LINE_COLOR = "#000000";
 
 
 // Set up wasm
@@ -45,6 +46,7 @@ const draw = () => {
     ctx.clearRect(1, 1, WIDTH - 2,HEIGHT - 2);
 
     // Display state of wasm
+    // XXX Conditionally draw each of these
     // Inputs
     {
         const len = core.input_length();
@@ -61,10 +63,54 @@ const draw = () => {
     }
 
     // Points
+    {
+        const len = core.points_length();
+        const x = new Int32Array(memory.buffer, core.points_x(), len);
+        const y = new Int32Array(memory.buffer, core.points_y(), len);
+        for (let i = 0; i < len; i++) {
+            ctx.beginPath();
+
+            ctx.arc(x[i], y[i], POINT_SIZE, 0, 2 * Math.PI);
+
+            ctx.fillStyle = POINT_COLOR;
+            ctx.fill();
+        }
+    }
 
     // Segments
+    {
+        const len = core.segs_length();
+        const x = new Int32Array(memory.buffer, core.segs_x(), len * 2);
+        const y = new Int32Array(memory.buffer, core.segs_y(), len * 2);
+        for (let i = 0; i < len; i += 2) {
+            ctx.beginPath();
+
+            ctx.moveTo(x[i], y[i]);
+            ctx.lineTo(x[i + 1], y[i + 1]);
+
+            ctx.lineWidth = LINE_WIDTH;
+            ctx.strokeStyle = LINE_COLOR;
+            ctx.stroke();
+        }
+    }
 
     // Rays
+    {
+        const len = core.rays_length();
+        const x = new Int32Array(memory.buffer, core.rays_x(), len * 2);
+        const y = new Int32Array(memory.buffer, core.rays_y(), len * 2);
+        for (let i = 0; i < len; i += 2) {
+            ctx.beginPath();
+
+            ctx.moveTo(x[i], y[i]);
+            // XXX Where boundary and line intersect towards this point
+            ctx.lineTo(x[i + 1], y[i + 1]);
+
+            ctx.lineWidth = LINE_WIDTH;
+            ctx.strokeStyle = LINE_COLOR;
+            ctx.stroke();
+        }
+    }
 
     // Other
 
