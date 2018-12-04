12dc45d6bc4548c4ae6e31945e1d3daf0e916066
Bownairo <eerokelly@gmail.com>
2018-12-04 17:12:37 -0500

    Move output out so it can be used by other projects

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,13 @@
 extern crate cfg_if;
 extern crate wasm_bindgen;
 extern crate good;
+extern crate webgeo_output;
 
 mod utils;
 
 use cfg_if::cfg_if;
 use wasm_bindgen::prelude::*;
+use webgeo_output::*;
 
 cfg_if! {
     // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
@@ -14,48 +16,6 @@ cfg_if! {
         extern crate wee_alloc;
         #[global_allocator]
         static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-    }
-}
-
-struct Points {
-    x: Vec<i32>,
-    y: Vec<i32>,
-}
-
-impl Points {
-    fn new() -> Self {
-        Points { x: Vec::new(), y: Vec::new() }
-    }
-
-    fn add(&mut self, x: i32, y: i32) {
-        self.x.push(x);
-        self.y.push(y);
-    }
-
-    fn len(&self) -> u32 {
-        self.x.len() as u32
-    }
-}
-
-struct Segs {
-    x: Vec<i32>,
-    y: Vec<i32>,
-}
-
-impl Segs {
-    fn new() -> Self {
-        Segs { x: Vec::new(), y: Vec::new() }
-    }
-
-    fn add(&mut self, x1: i32, y1: i32, x2: i32, y2: i32) {
-        self.x.push(x1);
-        self.x.push(x2);
-        self.y.push(y1);
-        self.y.push(y2);
-    }
-
-    fn len(&self) -> u32 {
-        (self.x.len() / 2) as u32
     }
 }
 
@@ -91,31 +51,13 @@ impl Config {
     }
 }
 
-pub struct Input {
+struct Input {
     points: Points,
 }
 
 impl Input {
     fn new() -> Self {
         Input { points: Points::new() }
-    }
-}
-
-pub struct Output {
-    points: Points,
-    segs: Segs,
-    rays: Segs,
-    stepping: bool,
-    halt: u8,
-}
-
-impl Output {
-    fn new() -> Self {
-        Output { points: Points::new(), segs: Segs::new(), rays: Segs::new(), stepping: false, halt: 0 }
-    }
-
-    fn stepping(&self) -> bool {
-        self.stepping
     }
 }
 
@@ -141,11 +83,11 @@ impl Core {
     }
 
     pub fn input_points_x(&self) -> *const i32 {
-        self.input.points.x.as_ptr()
+        self.input.points.x()
     }
 
     pub fn input_points_y(&self) -> *const i32 {
-        self.input.points.y.as_ptr()
+        self.input.points.y()
     }
 
     pub fn points_length(&self) -> u32 {
@@ -153,11 +95,11 @@ impl Core {
     }
 
     pub fn points_x(&self) -> *const i32 {
-        self.output.points.x.as_ptr()
+        self.output.points.x()
     }
 
     pub fn points_y(&self) -> *const i32 {
-        self.output.points.y.as_ptr()
+        self.output.points.y()
     }
 
     pub fn segs_length(&self) -> u32 {
@@ -165,11 +107,11 @@ impl Core {
     }
 
     pub fn segs_x(&self) -> *const i32 {
-        self.output.segs.x.as_ptr()
+        self.output.segs.x()
     }
 
     pub fn segs_y(&self) -> *const i32 {
-        self.output.segs.y.as_ptr()
+        self.output.segs.y()
     }
 
     pub fn rays_length(&self) -> u32 {
@@ -177,15 +119,15 @@ impl Core {
     }
 
     pub fn rays_x(&self) -> *const i32 {
-        self.output.rays.x.as_ptr()
+        self.output.rays.x()
     }
 
     pub fn rays_y(&self) -> *const i32 {
-        self.output.rays.y.as_ptr()
+        self.output.rays.y()
     }
 
     pub fn halt(&self) -> *const u8 {
-        &self.output.halt
+        self.output.halt()
     }
 
     pub fn state(&self) -> u8 {
@@ -198,8 +140,7 @@ impl Core {
 
     pub fn start(&mut self) {
         if self.config.run() {
-            let mut points = Vec::new();
-            self.input.points.x.iter().enumerate().for_each(|x| points.push((*x.1, self.input.points.y[x.0])));
+            let mut points = self.input.points.condense();
             // XXX Break output into it's own thing, then use output in good.
             good::run(points, &mut self.output);
             self.config.end();
